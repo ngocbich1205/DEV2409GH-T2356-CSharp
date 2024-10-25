dc92d69ebea00f5ba1c1fdb63a7cb917076edf81
ngocbich1205 <tranbich0503@gmail.com>
2024-10-25 08:57:40 +0700

    lab08

---
+++ lab08/lab 8.2/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_8._2
+{
+    internal class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ lab08/lab 8.3/Film.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_8._3
+{
+    internal class Film
+    {
+        public string FilmId { get; set; }
+        public string FilmName { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            return FilmId +":"+FilmName+":"+Price;
+        }
+    }
+}
